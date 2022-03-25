395fccc19644350ee254ae4265adb7c407f2ded1
Arthur-Agadzhanyan <artur0505gold@gmail.com>
2022-03-25 23:45:26 +0300

    adapted

--- src/js/main_page.js
+++ src/js/main_page.js
@@ -1,10 +1,11 @@
-const mainPageTextSlider = new Swiper('#main_page-intro_text_slider',{
+//Intro
+const mainPageTextSlider = new Swiper('#main_page-intro_text_slider', {
     slidesPerView: 1,
     loop: true,
     loopedSlides: 1,
     centeredSlides: true,
     speed: 1300,
-    pagination:{
+    pagination: {
         clickable: true,
         type: 'bullets',
         el: '#main_page-intro_slider_pagination',
@@ -17,13 +18,13 @@ const mainPageTextSlider = new Swiper('#main_page-intro_text_slider',{
     }
 })
 
-const mainPageImageSlider = new Swiper('#main_page-intro_image_slider',{
+const mainPageImageSlider = new Swiper('#main_page-intro_image_slider', {
     slidesPerView: 1,
     loop: true,
     loopedSlides: 1,
     centeredSlides: true,
     speed: 1300,
-    navigation:{
+    navigation: {
         prevEl: '#main_page-intro_slider-arrow_prev',
         nextEl: '#main_page-intro_slider-arrow_next'
     }
@@ -31,3 +32,30 @@ const mainPageImageSlider = new Swiper('#main_page-intro_image_slider',{
 
 mainPageTextSlider.controller.control = mainPageImageSlider
 mainPageImageSlider.controller.control = mainPageTextSlider
+
+//News
+
+const mainPageNewsMbSlider = new Swiper('#main_page-news_mb_slider', {
+    slidesPerView: 1,
+    loop: true,
+    loopedSlides: 1,
+    speed: 1300,
+    autoplay: {
+        delay: 2500
+    },
+    pagination: {
+        clickable: true,
+        type: 'bullets',
+        el: '#main_page-news_slider_pagination',
+        bulletElement: 'div',
+        bulletClass: 'pagination__item',
+        bulletActiveClass: 'pagination__item_active',
+        renderBullet: function (index, className) {
+            return '<div class="' + className + '">*</div>';
+        }
+    },
+    navigation: {
+        prevEl: '#news_slider_arrow_left',
+        nextEl: '#news_slider_arrow_right'
+    }
+})
