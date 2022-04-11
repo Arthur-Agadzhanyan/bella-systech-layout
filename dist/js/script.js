f62245128b8ebea092b15a6a979b1b009da963b6
Arthur-Agadzhanyan <artur0505gold@gmail.com>
2022-04-12 01:22:18 +0300

    changed: navigation bar

--- dist/js/script.js
+++ dist/js/script.js
@@ -36,6 +36,14 @@ burgerBtn.addEventListener('click',(e)=>{
     burgerBtn.classList.toggle('contacts__burger_active');
     menuBar.classList.toggle('menu_active');
 
+    if(menuBar.classList.contains('bot_0')){
+        menuBar.classList.remove("bot_0")
+    }else{
+        setTimeout(()=>{
+            menuBar.classList.add("bot_0")
+        },400)
+    } 
+
     document.body.classList.toggle('overf_hidden')
 });
 //Intro
--- src/js/navbar.js
+++ src/js/navbar.js
@@ -5,5 +5,13 @@ burgerBtn.addEventListener('click',(e)=>{
     burgerBtn.classList.toggle('contacts__burger_active');
     menuBar.classList.toggle('menu_active');
 
+    if(menuBar.classList.contains('bot_0')){
+        menuBar.classList.remove("bot_0")
+    }else{
+        setTimeout(()=>{
+            menuBar.classList.add("bot_0")
+        },400)
+    } 
+
     document.body.classList.toggle('overf_hidden')
 })
